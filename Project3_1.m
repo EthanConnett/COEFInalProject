b60030f23cd4542f49e9d7f4dc49c76128a2e350
P3yt0n <37157455+P3yt0n@users.noreply.github.com>
2021-12-05 19:05:12 -0600

    Add files via upload

--- Project3_1.m
+++ Project3_1.m
@@ -1,7 +1,16 @@
 n = 4;
 T= 1;
 deltat = 10.^-5;
-ft = [0,0,0,0];
+N = floor(T/deltat);
+t= linspace(0,T,N);
+x = zeros(4,N);
 u0 = [1,0,0,0];
+x(:,1) = u0;
+%dudt = A*u(t) + f(t);
+f = [0,0,0,0];
 A = [-2,1,0,0; 1,-2,1,0;0,1,-2,1;0,1,-1,0];
 
+for i = 2:N
+    D = A * x(i-1) + f(i-1)
+    test = deltat * D + x(i-1)
+end
